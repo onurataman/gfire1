b543a1a8bd02a9abb2e7e19e060b8e661582960b
onurataman <70483728+onurataman@users.noreply.github.com>
2025-05-13 00:34:49 +0000

    Title Change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'TipEase',
+  title: 'Ataman - TipEase',
   description: 'Calculate tips and split bills with ease.',
 };
 
